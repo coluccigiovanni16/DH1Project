9cbb1f8912a1d35a49cd5ab004c91e719a1eb5f6
Vlr17 <32646239+Vlr17@users.noreply.github.com>
2019-03-05 18:30:04 +0100

    server modifica metodo main in open

--- src/ThreadedEchoServer.java
+++ src/ThreadedEchoServer.java
@@ -1,6 +1,7 @@
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 
 
 public class ThreadedEchoServer implements Runnable {
@@ -12,7 +13,7 @@ public class ThreadedEchoServer implements Runnable {
         sock = s;
     }
 
-    public static void main(String args[]) throws IOException {
+    public void open () throws IOException {
         ServerSocket serv = new ServerSocket(PORT);
         while (true) {
             Socket sock = serv.accept();
@@ -23,9 +24,9 @@ public class ThreadedEchoServer implements Runnable {
     }
     public void run() {
         try {
-            BufferedReader brd = new BufferedReader(new InputStreamReader(sock.getInputStream(), "UTF-16"));
+            BufferedReader brd = new BufferedReader(new InputStreamReader(sock.getInputStream(), StandardCharsets.UTF_16));
             String s = brd.readLine();
-            PrintWriter prw = new PrintWriter(new OutputStreamWriter(sock.getOutputStream(), "UTF-16"));
+            PrintWriter prw = new PrintWriter(new OutputStreamWriter(sock.getOutputStream(), StandardCharsets.UTF_16));
             prw.print(s);
             prw.println(s);
             prw.flush();
